2adb6e920fd40261fbaae5e7f6ee6e34b95c74f1
Rodrigo Gaona Gonzalez <rodrigogaonagonzalez@gmail.com>
2020-05-29 18:24:06 -0400

    Fixed project setup

--- config/data.php
+++
@@ -1,9 +0,0 @@
-<?php
-  $user1 = 'grupo84';
-  $password1 = 'grupo84';
-  $databaseName1 = 'grupo84e3';
-
-  $user2 = 'grupo15';
-  $password2 = 'grupo15';
-  $databaseName2 = 'grupo15e3';
-?>
--- usuario/logout.php
+++
@@ -1,6 +0,0 @@
-<?php require("../config/conexion.php");
-  session_start();
-  if (session_destroy()){
-    header("Location: ../index_par.php");
-  }
-?>
