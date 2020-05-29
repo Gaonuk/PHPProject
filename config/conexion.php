27e3777cec547f3003e0116d0d8cd8ec8775fe74
Rodrigo Gaona Gonzalez <rodrigogaonagonzalez@gmail.com>
2020-05-29 17:53:14 -0400

    New PDO Connections to Heroku dbs

--- config/conexion.php
+++ config/conexion.php
@@ -1,11 +1,26 @@
 <?php
   try {
     #Pide las variables para conectarse a la base de datos.
-    require('data.php');
+		$db = parse_url(getenv("DATABASE_URL"));
+		$db2 = parse_url(getenv("HEROKU_POSTGRESQL_PURPLE_URL"));
+	
+		$db_impar = new PDO("pgsql:" . sprintf(
+				"host=%s;port=%s;user=%s;password=%s;dbname=%s",
+				$db["host"],
+				$db["port"],
+				$db["user"],
+				$db["pass"],
+				ltrim($db["path"], "/")
+			));
+		$db_par = new PDO("pgsql:" . sprintf(
+				"host=%s;port=%s;user=%s;password=%s;dbname=%s",
+				$db2["host"],
+				$db2["port"],
+				$db2["user"],
+				$db2["pass"],
+				ltrim($db2["path"], "/")
+			));
     # Se crea la instancia de PDO
-    $db_par = new PDO("pgsql:dbname=$databaseName1;host=146.155.13.72;port=5432;user=$user1;password=$password1");
-    $db_impar = new PDO("pgsql:dbname=$databaseName2;host=146.155.13.72;port=5432;user=$user2;password=$password2");
-
   } catch (Exception $e) {
     echo "No se pudo conectar a la base de datos: $e";
   }
